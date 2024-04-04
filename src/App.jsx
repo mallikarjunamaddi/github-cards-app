287fa16c65e6f2f8c511f46f41c9cc4b379f0de3
Mallikarjuna Maddi <rammallikarjunarao@gmail.com>
2024-04-04 20:26:38 +0530

    integrated all the components in the App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "./index.css"; // This is how we import CSS files in React.
-import Card from "./Card";
+import CardList from "./CardList";
+import InputForm from './InputForm'
+
+// Source Github API for below data https://api.github.com/users/mallikarjunamaddi
+const userProfiles = [
+    {avatar_url: "https://avatars.githubusercontent.com/u/22195101?v=4", name: "Mallikarjuna Maddi", company: "@microsoft"},
+    {avatar_url: "https://avatars.githubusercontent.com/u/25254?v=4", name: "TJ Holowaychuk", company: "Apex"},
+];
 
 class App extends React.Component {
+    state = {
+        profiles: userProfiles
+    };
+
+    handleInputFromSubmit = (user) => {
+       this.setState(prevState => ({
+         profiles: [...prevState.profiles, user]
+       }));
+    }
+
     render() {
         return (
             <div>
                 <div className="header">{this.props.title}</div>
-                <Card />
+                <InputForm onSubmit={this.handleInputFromSubmit}/>
+                <CardList users={this.state.profiles}/>
             </div>
         );
     }
